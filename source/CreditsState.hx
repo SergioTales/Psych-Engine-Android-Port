a70914ef93d0d4672b40260e55a688ea3dae164e
SergioTales <95892063+SergioTales@users.noreply.github.com>
2021-12-11 12:23:32 -0300

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -47,7 +47,7 @@ class CreditsState extends MusicBeatState
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
 			['Android Port'],
-			['SergioTales',		    'mauro',		    'otimizador fodao',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['sergiotales',		    'mauro',		    'otimizador fodao',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
